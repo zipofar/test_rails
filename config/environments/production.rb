3f34df9c67f4305d2c35e3ff8d08c0554ab21089
Evgeny <evgeny.agafonov@dualbootpartners.com>
2022-06-22 19:19:29 +0300

    Add uffizzi compose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.hosts = [/.*\.com/, IPAddr.new("0.0.0.0/0"), 'localhost']
 end
